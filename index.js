56cd0d17287c15644e8ee66cdab99da676e7eb54
Andreyjkov <Andreyjskov@gmail.com>
2022-06-09 15:42:01 +0300

    feat: bubble sort

---
+++ index.js
@@ -0,0 +1,17 @@
+// Пузырьковая сортировка
+const bubble = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1; j++) {
+            if(arr[j] > arr[j+1]) {
+                const temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+            
+        }
+        
+    }
+    return arr
+}
+
+console.log(bubble([1,2,7,5,9,8,6,3,4,0]));
